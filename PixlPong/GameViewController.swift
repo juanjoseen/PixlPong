2e84d4c0cdc92410b5ec56f21bfef634b6989796
Juan <juanjoseen@gmail.com>
2018-08-09 15:55:48 -0500

    - added GlobalData class to save all the data in the game - added last score and max score at Gameover scene - prepared for dynamic color change in ball & bars

--- PixlPong/GameViewController.swift
+++ PixlPong/GameViewController.swift
@@ -15,7 +15,7 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
+        // Load 'StartScene.sks' as a GKScene. This provides gameplay related content
         // including entities and graphs.
         if let scene = GKScene(fileNamed: "StartScene") {
             
@@ -35,8 +35,8 @@ class GameViewController: UIViewController {
                     
                     view.ignoresSiblingOrder = true
                     
-                    view.showsFPS = true
-                    view.showsNodeCount = true
+                    view.showsFPS = false
+                    view.showsNodeCount = false
                 }
             }
         }
